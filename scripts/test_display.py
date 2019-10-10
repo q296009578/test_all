7a9385f23b0017cf9f9a788246d3b0f800b75eed
q296009578 <50868686+q296009578@users.noreply.github.com>
2019-10-10 08:41:44 +0800

    000
    000

--- scripts/test_display.py
+++ scripts/test_display.py
@@ -2,12 +2,12 @@
 class Test_abc:
     def test_aaa(self):
         print(111)
-        assert 1
+        assert 0
 
     def test_bbb(self):
         print(222)
-        assert 1
+        assert 0
 
     def test_ccc(self):
         print(333)
-        assert 0
+        assert 1
